f1f39221c61b54c9c67a363928bff19468ae1519
Fol4 <ana17110144@gmail.com>
2023-04-14 12:48:18 +0300

    snake v0.1

--- 2sem/PyQt/5. snake/snake.py
+++ 2sem/PyQt/5. snake/snake.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtGui, QtCore, QtWidgets
+import random
 
 
 class Snake:
@@ -9,7 +10,9 @@ class Snake:
 
         self.direction = 'left'
         self.grow = False
-        self.color = QtGui.QColor(0x538846)
+        self.color = QtGui.QImage('food/body.jpg')
+        self.color_head = QtGui.QImage('food/head.jpg')
+        self.color_ass = QtGui.QImage('food/ass.jpg')
 
         self.width = width
         self.height = height
@@ -22,7 +25,7 @@ class Snake:
         if self.direction == 'left':
             self.head = [self.head[0] - 1, self.head[1]]
 
-            if self.head[0] == 0:
+            if self.head[0] == -1:
                 self.head[0] = self.width - 1
 
         if self.direction == 'right':
@@ -34,7 +37,7 @@ class Snake:
         if self.direction == 'up':
             self.head = [self.head[0], self.head[1] - 1]
 
-            if self.head[1] == 0:
+            if self.head[1] == -1:
                 self.head[1] = self.height - 1
 
         if self.direction == 'down':
@@ -47,9 +50,12 @@ class Snake:
 
         if not self.grow:
             self.body.pop()
+        else:
+            self.grow = False
+
 
 class Food:
-    COLOR = [QtGui.QColor(0xcb3333), QtGui.QColor(0xf30a66), QtGui.QColor(0x5188c4)]
+    COLOR = [QtGui.QImage('food/chich.png'), QtGui.QImage('food/bour_smiling.png')]
 
     def __init__(self, width, height):
         self.bag = []
@@ -58,14 +64,20 @@ class Food:
         self.height = height
 
     def drop(self):
-        pass
+        random.seed(random.randint(10, 40))
+
+        x = random.randint(0, self.width - 1)
+        y = random.randint(0, self.height - 1)
+        color = random.randint(0, len(Food.COLOR) - 1)
+
+        self.bag.append({'cord': [x, y], 'color': Food.COLOR[color]})
 
 
 class Board(QtWidgets.QFrame):
     SPEED = 80
 
-    HEIGHTINBLOCKS = 40
-    WIDTHINBLOCKS = 60
+    HEIGHTINBLOCKS = 10
+    WIDTHINBLOCKS = 15
 
     def __init__(self, parent):
         super(Board, self).__init__(parent)
@@ -75,6 +87,7 @@ class Board(QtWidgets.QFrame):
         self.timer = QtCore.QBasicTimer()
 
         self.snake = Snake(Board.WIDTHINBLOCKS, Board.HEIGHTINBLOCKS)
+        self.food = Food(Board.WIDTHINBLOCKS, Board.HEIGHTINBLOCKS)
 
     def block_width(self):
         return self.frameGeometry().width() / Board.WIDTHINBLOCKS
@@ -89,19 +102,45 @@ class Board(QtWidgets.QFrame):
         rect = self.contentsRect()
 
         boardtop = rect.bottom() - self.frameGeometry().height()
+        self.drawrect(painter, rect.left() + self.snake.body[0][0] * self.block_width(),
+                      boardtop + self.snake.body[0][1] * self.block_height(), self.snake.color_head)
+        self.drawrect(painter, rect.left() + self.snake.body[-1][0] * self.block_width(),
+                      boardtop + self.snake.body[-1][1] * self.block_height(), self.snake.color_ass)
 
         for cord in self.snake.body:
             self.drawrect(painter, rect.left() + cord[0] * self.block_width(),
                           boardtop + cord[1] * self.block_height(), self.snake.color)
 
+        for fruit in self.food.bag:
+            cord = fruit['cord']
+            self.drawrect(painter, rect.left() + cord[0] * self.block_width(),
+                          boardtop + cord[1] * self.block_height(), fruit['color'])
+
     def drawrect(self, painter, x, y, color):
-        painter.fillRect(x, y, self.block_width() - 2, self.block_height() - 2, color)
+        rect = QtCore.QRect(x, y, self.block_width() - 2, self.block_height() - 2)
+        painter.drawImage(rect, color)
 
     def timerEvent(self, a0: QtCore.QTimerEvent) -> None:
 
         if a0.timerId() == self.timer.timerId():
             self.snake.move()
+            self.collision()
+            self.drop()
             self.update()
+
+    def collision(self):
+        for fruit in self.food.bag:
+            if fruit['cord'] == self.snake.head:
+                print(1)
+                self.snake.grow = True
+                print(2)
+                self.food.bag.remove(fruit)
+                print(3)
+                break
+
+    def drop(self):
+        if len(self.food.bag) == 0:
+            self.food.drop()
 
     def keyPressEvent(self, a0: QtGui.QKeyEvent) -> None:
 
